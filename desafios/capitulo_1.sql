8114124f1e1726626cafb3c35d6fa86d8402c09c
a6santa <a6santa@outlook.com>
2020-11-08 23:28:41 -0300

    Create capitulo_1.sql

---
+++ desafios/capitulo_1.sql
@@ -0,0 +1,13 @@
+/** 1- Lista de Livros Favoritos
+Quais seus Livros favoritos? Crie uma tabela para guardar os registros dos seus livros favoritos contendo as seguintes colunas:
+id
+título
+autor
+nota
+categoria
+data_leitura
+Após a criação da tabela, favor realizar a inserção de pelo menos 5 livros
+
+2 - Consulte sua categorias favoritas
+Com base em sua tabela crie uma query que retorne o livro da sua categoria favorita.
+**/
