3813e4bf4a88457e5b96d7aec3788879facb682a
a6santa <a6santa@outlook.com>
2020-11-08 23:29:51 -0300

    Create capitulo_2.sql

---
+++ desafios/capitulo_2.sql
@@ -0,0 +1,7 @@
+/**
+
+Como você viu criamos um data mart para o time de vendas, mas após a fase de implementação o time do comercial solicitou um reajuste no projeto. 
+Eles perceberam que para entender melhor as vendas é preciso também entender o cliente. Para isso é preciso adicionar no data mart a dimensão de 
+clientes com os dados que você acha pertinente e não se esqueça de referenciá lo na tabela fato. Para esse desafio sugiro o uso do vertabelo.
+
+**/
